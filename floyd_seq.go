5f8dca277e64f515771aaee66f3ae1826232b55b
Warren Wang <warrenwangtennis@gmail.com>
2021-05-06 22:26:04 -0500

    floyd sequential

--- floyd_seq.go
+++ floyd_seq.go
@@ -1,18 +1,59 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 )
 
-func main() {
-	n, m, adj := read_input("t1.txt")
+var n, m int
+var adj []map[int]int
+var adj_mat, apsp [][]int
 
-	fmt.Println(n, m, adj)
-
-	var mat = [][]int{
-		{0, 1, 2, 3},
-		{4, 5, 6, 7},
+func load_adj_mat() [][]int {
+	adj_mat = make([][]int, n)
+	for i := 0; i < n; i++ {
+		adj_mat[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			if i == j {
+				adj_mat[i][j] = 0
+			} else {
+				adj_mat[i][j] = INF
+			}
+		}
+	}
+	
+	for u := 0; u < n; u++ {
+		for v, d := range adj[u] {
+			adj_mat[u][v] = d
+		}
 	}
 
-	write_output("./floyd_seq_out.txt", mat)
+	return adj_mat
+}
+
+func solve() {
+	apsp = make([][]int, n)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			apsp[i] = append(apsp[i], adj_mat[i][j])
+		}
+		assert(len(apsp[i]) == n)
+	}
+
+	for k := 0; k < n; k++ {
+		for i := 0; i < n; i++ {
+			for j := 0; j < n; j++ {
+				apsp[i][j] = min(apsp[i][j], apsp[i][k] + apsp[k][j])				
+			}
+		}
+	}
+}
+
+func main() {
+	n, m, adj = read_input("t2.txt")
+
+	load_adj_mat()
+
+	solve()
+
+	write_output("./floyd_seq_out.txt", apsp)
 }
